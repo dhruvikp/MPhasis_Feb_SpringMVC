a96cc1e1d0e1f76f07706a3b001b87ffc75b229a
Dhruvik <dhruviksparikh@gmail.com>
2022-03-21 06:48:33 +0000

    Added changes

---
+++ src/main/java/com/simplilearn/MphasisFebSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MphasisFebSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MphasisFebSpringMvcApplication.class, args);
+	}
+
+}

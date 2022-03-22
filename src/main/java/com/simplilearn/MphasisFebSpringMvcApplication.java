448ddba00d33b04f52f0fb44826fd292a0f7a92a
Dhruvik <dhruviksparikh@gmail.com>
2022-03-22 05:07:20 +0000

    Added changes

--- src/main/java/com/simplilearn/MphasisFebSpringMvcApplication.java
+++ src/main/java/com/simplilearn/MphasisFebSpringMvcApplication.java
@@ -2,12 +2,18 @@ package com.simplilearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MphasisFebSpringMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MphasisFebSpringMvcApplication.class, args);
+		ConfigurableApplicationContext appCon = SpringApplication.run(MphasisFebSpringMvcApplication.class, args);
+		
+		/*appCon.start();
+		
+		appCon.stop();*/
+	
 	}
 
 }
